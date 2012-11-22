c3e41b40cf0de64dbe7d8d0e7077fb89f586a021
chenlei <chenlei@shendu.com>
2012-11-22 13:39:34 +0800

    fix lights color

--- liblight/lights.c
+++ liblight/lights.c
@@ -233,8 +233,8 @@ static int set_light_leds(struct light_state_t const *state, int type)
     if(type == 1){
         pthread_mutex_lock(&g_lock);
         par.RED = ((state->color >> 16) & 0xFF)? 255 : 0;
-        par.BLUE = ((state->color >> 8) & 0xFF)? 255 : 0;
-        par.GREEN = (state->color & 0xFF)? 255 : 0;
+        par.GREEN = ((state->color >> 8) & 0xFF)? 255 : 0;
+        par.BLUE = (state->color & 0xFF)? 255 : 0;
         par.onMs = state->flashOnMS;
         par.offMs = state->flashOffMS;
         pthread_mutex_unlock(&g_lock);
